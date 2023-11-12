612d4ee4898a4f9f76b859b906be1f4ccb35ea76
SkulZOnTheYT <92020118+SkulZOnTheYT@users.noreply.github.com>
2023-11-12 12:56:13 +0700

    Update tiktok.js

--- plugins/download/tiktok.js
+++ plugins/download/tiktok.js
@@ -1,21 +1,22 @@
- let handler = async (m, { conn, text, args }) => {         
-    if (!/https?:\/\/(www\.|v(t|m|vt)\.|t\.)?tiktok\.com/i.test(args[0])) return m.reply(`Example : #tiktok https://vt.tiktok.com/ZSwWCk5o/`)
-    await m.reply("Loading...")
-    let req = await fetch(global.API('xfarr', '/api/download/tiktoknowm', { url: args[0] }, 'apikey'))
-    let json = await req.json()
-    if (json.status !== 200) return m.reply("sukses")
-    console.log(json.result.url)
-    conn.sendFile(m.chat, json.result.url, `${json.result.description}.mp4`, '*[ TIKTOK VIDEO DOWNLOADER ]*', m)
-    if (/music/g.test(json.result.url)) {
-       let req = await fetch(global.API('xfarr', '/api/download/tiktokslide', { url: args[0] }, 'apikey'))
-       let json = await req.json()
-       for (let url of json.result.url) {
-       console.log(url)
-       await conn.sendMsg(m.chat, { image: { url: url }, caption: '*[ TIKTOK SLIDE DOWNLOADER ]*' }, { quoted: m })      
-      }
-     }
-    }
+import { youtubedl } from '@bochilteam/scraper-sosmed'
+import ytdl from 'ytdl-core'
+import { niceBytes } from '../../lib/func.js'
+import { soundcloud } from '../../lib/scrape.js'
+import db from '../../lib/database.js'
 
+export async function before(m, {conn, body}) {
+let chat = db.data.settings[conn.user.jid]
+if (chat.autodl) {
+	if (/https?:\/\/(www\.|v(t|m|vt)\.|t\.)?tiktok\.com/i.test(m.text)) {
+  // return m.reply(`Example : #tiktok https://vt.tiktok.com/ZSwWCk5o/`)
+    await m.reply("Loading...")
+    let req = await fetch(global.API('xfarr', '/api/download/tiktoknowm', { url: m.text }, 'apikey')) //GET INFO TIKTOK NOWM
+    let json = await req.json()
+    console.log(json.result.url)
+    conn.sendFile(m.chat, json.result.url, `${json.result.description}.mp4`, '[ TIKTOK VIDEO DOWNLOADER ]', m)
+   }
+  }
+ }
          
 handler.help = ['tiktok <url>','tiktokdl <url>', 'tiktokslide <url>']
 handler.tags = ['download']
